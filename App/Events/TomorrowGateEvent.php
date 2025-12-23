2d5876101c53ab768c776438cc8639976a2dbcd3
Ricardo Sousa <rmbsousa@gmail.com>
2025-12-23 00:25:08 +0000

    Bugfix: tomorrow arrived early ;)

--- App/Events/TomorrowGateEvent.php
+++ App/Events/TomorrowGateEvent.php
@@ -4,29 +4,29 @@ declare(strict_types=1);
 
 namespace App\Events;
 
-use DateTime;
 use Flows\Contracts\Gates\Frequent as FrequentContract;
 use Flows\Contracts\Gates\GateEvent as GateEventContract;
 
 class TomorrowGateEvent implements FrequentContract, GateEventContract
 {
+    /**
+     * @var int this time tomorrow
+     */
+    private int $tomorrow;
+
     public function __construct(
-        /**
-         * @var DateTime this time tomorrow
-         */
-        private DateTime $tomorrow = new DateTime('+1 day'),
         /**
          * @var float frequency to check for modifications, in seconds
          */
         private float $frequency = 1.5
-    ) {}
+    ) {
+        $this->tomorrow = strtotime('tomorrow');
+    }
 
     public function resolve($data = null): bool
     {
         // Because tomorrow never comes ;)
-        return $this->tomorrow
-            ->diff(new DateTime('now'))
-            ->format('%a') === '0';
+        return strtotime('now') >= $this->tomorrow;
     }
 
     public function getFrequency(): float
