7e5047de3c5a37bae5e3143c41d9aa4a015859a0
Ricardo Sousa <rmbsousa@gmail.com>
2026-02-06 18:33:27 +0000

    Modified config settings scaffold file;

--- App/Config/app.php
+++ App/Config/app.php
@@ -1,21 +1,33 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'gate' => [
         'on_branch' => [
+            // When flow branches, keep previous process output
             'keep_io' => false,
         ],
     ],
-    'stop' => [
-        'on_no_event_handler' => false,
-        'on_offload_error' => true,
-    ],
     'http' => [
         'server' => [
-            'command_socket_path' => '/tmp/command.sock',
+            // HTTP helper server command socket path (for event gates with HTTP events)
+            'command_socket_path' => sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'flows-command.sock',
+            // HTTP helper server listens on this address
             'address' => '0.0.0.0:9090',
+            // Seconds, HTTP handler server timeout for a response from a Flows process
             'timeout_read_external_process' => 30,
         ],
     ],
-    'wait_timeout_for_files' => 1000, // microseconds, low value when running on fast hardware
+    'stop' => [
+        // Stop when an event that has no registered handler is triggered
+        'on_no_event_handler' => false,
+        // Stop when an error occurs trying to offload a process 
+        'on_offload_error' => true,
+    ],
+    /* Microseconds, timeout for a Flows process to wait for any file to be created 
+     * by an external process, internally used to wait for the command socket to be created
+     * during HTTP handler server boot (this value is used in a trait that "sleep"s the PHP thread).
+     * Low value when running on fast hardware */
+    'wait_timeout_for_files' => 300,
 ];
